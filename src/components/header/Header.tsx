cd20b3af0005ac5c81efafe3145af311e9172036
tural.shkrov <tural.shkrov@gmail.com>
2023-11-10 11:42:24 +0400

    Feat: Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,25 @@
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 import './Header.css'
 
 export default function Header() {
   return (
     <header>
-      <Navbar expand="lg" data-bs-theme="dark">
+      <Navbar expand="md" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Tural Shukurov</Navbar.Brand>
+          <Navbar.Brand>Tural Shukurov</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <NavDropdown title="AZE" id="basic-nav-dropdown" className='ms-3'>
+            <Nav className="ms-2 ms-md-auto py-3 py-md-0">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+              {/* <NavDropdown title="AZE" id="basic-nav-dropdown" className='ms-3'>
                 <NavDropdown.Item>AZE</NavDropdown.Item>
                 <NavDropdown.Item>ENG</NavDropdown.Item>
                 <NavDropdown.Item>DEU</NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap'
 import Header from '../../components/header/Header'
 import './Home.css'
 
@@ -5,6 +6,18 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <Container>
+        <div className="margin-top">
+          <h1 className='greeting'>Welcome 🖐</h1>
+          <p className='home-about-text ps-3 ps-md-4 py-4 my-4'>
+            My name is Tural Shukurov. I'm a jr front-end developer in Baku, Azerbaijan. 
+            I also study Master's of Artificial Intelligence at ASOIU.
+            I continue to learn and improve my knowledge and skills.
+            I enjoy working with React, TypeScript and other frameworks and libraries.
+          </p>
+        </div>
+        
+      </Container>
     </div>
   )
 }
