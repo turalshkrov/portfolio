fade0f685125ad01ce7ac6a5fd5763ba70bbaffa
tural.shkrov <tural.shkrov@gmail.com>
2023-11-15 20:46:43 +0400

    Fix: Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,31 +7,31 @@ export default function Footer() {
   return (
     <footer>
       <Container>
-        <div className="row py-3 justify-content-between align-items-center">
-          <div className="col-12 col-md-8">
+        <div className="row py-3 justify-content-center justify-content-md-between align-items-center">
+          <div className="col-11 col-md-8 mb-3 mb-md-0">
             <h5 className="fw-bold mb-3">TURAL SHUKUROV</h5>
             <p className="footer-text">A Frontend focused Web Developer building the Frontend of Websites and Web Applications</p>
           </div>
-          <div className="socials row col-12 col-md-2 justify-content-end">
-            <div className="col-12">
-              <h5 className="fw-bold align-left">SOCIALS</h5>
+          <div className="socials row col-11 col-md-2 justify-content-md-end">
+            <div className="col-12 mb-2 p-0 px-md-3">
+              <h5 className="fw-bold">SOCIALS</h5>
             </div>
-            <div className="col-3 d-flex justify-content-center justify-content-md-start">
+            <div className="col-2 col-md-3 p-0 px-md-3 d-flex justify-content-start">
               <a href="https://twitter.com/TuralSukurov4" target="_blank">
                 <FaTwitter size={20}/>
               </a>
             </div>
-            <div className="col-3 d-flex justify-content-center justify-content-md-start">
+            <div className="col-2 col-md-3 p-0 px-md-3 d-flex justify-content-start">
               <a href="https://github.com/turalshkrov" target="_blank">
                 <FaGithub size={20}/>
               </a>
             </div>
-            <div className="col-3 d-flex justify-content-center justify-content-md-start">
+            <div className="col-2 col-md-3 p-0 px-md-3 d-flex justify-content-start">
               <a href="https://www.linkedin.com/in/tural-shkrov/" target="_blank">
                 <FaLinkedinIn size={20}/>
               </a>
             </div>
-            <div className="col-3 d-flex justify-content-center justify-content-md-start">
+            <div className="col-2 col-md-3 p-0 px-md-3 d-flex justify-content-start">
               <a href="https://www.instagram.com/tural.shkrov/" target="_blank">
                 <FaInstagram size={20}/>
               </a>
