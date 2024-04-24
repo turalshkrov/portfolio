0e1c505eaec20c14813cfde24b953ba17a740ece
tural.shkrov <tural.shkrov@gmail.com>
2024-04-25 01:53:15 +0400

    add: new project & tools

--- src/components/toolCard/ToolCard.tsx
+++ src/components/toolCard/ToolCard.tsx
@@ -8,7 +8,7 @@ export type ToolType = {
 export default function ToolCard(prop: { tool: ToolType }) {
   const { tool } = prop
   return (
-    <div className='tool-item col-3 d-flex flex-column align-items-center justify-content-end mb-5' key={tool.title}>
+    <div className='tool-item col-3 col-md-2 d-flex flex-column align-items-center justify-content-end mb-5' key={tool.title}>
       <img src={tool['img-url']}/>
       <p className='mt-2 mt-md-3 mb-0 fw-bold'>{tool["title"]}</p>
     </div>
